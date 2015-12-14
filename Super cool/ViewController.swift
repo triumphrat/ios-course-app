25e1fc8832fcab09a713885ee9ac0286acef4370
Bernard Lupini <bernardlupini@gmail.com>
2015-12-14 11:29:50 -0500

    Initial Commit

---
+++ Super cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super cool
+//
+//  Created by Bernard Lupini on 12/14/15.
+//  Copyright © 2015 Bernard Lupini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
